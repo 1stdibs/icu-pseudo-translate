6ebd007622fe4912c159f6b4a45eb90afe9e1d50
Tim Whidden <tim@1stdibs.com>
2017-10-20 14:55:45 -0400

    initial commit

---
+++ src/lib/print.js
@@ -0,0 +1,51 @@
+// swiped from https://raw.githubusercontent.com/yahoo/react-intl/master/examples/translations/scripts/lib/printer.js
+// required a couple bug fixes
+const ESCAPED = {
+    '\\': '\\\\',
+    '\\#': '\\#',
+    '{': '\\{',
+    '}': '\\}'
+};
+const ESCAPE_REGEX = /\\#|[{}\\]/g;
+
+export default function printICUMessage(ast) {
+    return ast.elements.reduce((msg, el) => {
+        const { format, id, type, value } = el;
+
+        if (type === 'messageTextElement') {
+            return msg + value.replace(ESCAPE_REGEX, char => ESCAPED[char]);
+        }
+
+        if (!format) {
+            return msg + `{${id}}`;
+        }
+
+        let formatType = format.type.replace(/Format$/, '');
+        let style;
+        let offset;
+        let options;
+
+        switch (formatType) {
+            case 'number':
+            case 'date':
+            case 'time':
+                style = format.style ? `, ${format.style}` : '';
+                msg += `{${id}, ${formatType}${style}}`;
+                break;
+
+            case 'plural':
+            case 'select':
+                offset = format.offset ? `, offset:${format.offset}` : '';
+                options = format.options.reduce((str, option) => {
+                    const optionValue = printICUMessage(option.value);
+                    return str + ` ${option.selector} {${optionValue}}`;
+                }, '');
+                if (format.ordinal && formatType === 'plural') {
+                    formatType = 'selectordinal';
+                }
+                msg += `{${id}, ${formatType}${offset},${options}}`;
+                break;
+        }
+        return msg;
+    }, '');
+}
